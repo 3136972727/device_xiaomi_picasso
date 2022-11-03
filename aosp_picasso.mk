65faedb7e445aad2766028e3f740636e00e00121
3136972727 <78227233+3136972727@users.noreply.github.com>
2022-11-04 06:55:46 +0800

    Update and rename lineage_picasso.mk to aosp_picasso.mk

--- aosp_picasso.mk
+++ aosp_picasso.mk
@@ -11,35 +11,27 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from picasso device
 $(call inherit-product, device/xiaomi/picasso/device.mk)
 
-# Inherit some common LineageOS  stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common PixelExperience stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+TARGET_USES_AOSP_RECOVERY := true
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_INCLUDE_LIVE_WALLPAPERS := false
+TARGET_SUPPORTS_QUICK_TAP := true
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_picasso
+PRODUCT_NAME := aosp_picasso
 PRODUCT_DEVICE := picasso
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi K30 5G
 PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    BUILD_DISPLAY_ID="lineage"
-
-# Blur
-TARGET_ENABLE_BLUR := true
 
 # Boot animation
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_SCREEN_HEIGHT := 2400
 TARGET_SCREEN_WIDTH := 1080
 
-# Face unlock
-TARGET_SUPPORT_FACE_UNLOCK := true
-
 # GMS
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
-# Maintainter
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.maintainer.name=nmou
 
-# Quick tap
-TARGET_SUPPORTS_QUICK_TAP := true
+
